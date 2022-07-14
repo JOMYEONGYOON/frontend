a31222da3523b3c24bf741c69df7ae7c3922befa
JOMYEONGYOON <hairhold@naver.com>
2022-07-14 17:37:55 +0900

    second

--- my-deploy/src/App.js
+++ my-deploy/src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/deploy")
+      .then((response) => response.text())
+      .then((msg) => {
+        setMsg(msg);
+      });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          {msg}
         </p>
         <a
           className="App-link"
