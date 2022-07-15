b6a43e9a668d93460613f95429dbd0f8a966ee2a
JOMYEONGYOON <hairhold@naver.com>
2022-07-15 10:55:23 +0900

    server 연동

--- my-deploy/src/setupProxy.js
+++ my-deploy/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://3.101.146.109:8080",
       changeOrigin: true,
     })
   );
